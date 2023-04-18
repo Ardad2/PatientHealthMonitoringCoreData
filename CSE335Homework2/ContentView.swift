32dde6a1244a609fc5d25f212e66f7b8ec384adf
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-18 16:35:38 -0700

    created home screen

--- CSE335Homework2/ContentView.swift
+++ CSE335Homework2/ContentView.swift
@@ -9,13 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView{
+            VStack(spacing:100) {
+                
+                //This button can be used for testing outputs.
+                //Do note that for swift multiplication, 100 * 0.1 = 110.00000000000001
+                //Also note data is kept track off for the "previous consecutive days", if a day is missing then the health indicators are resetted.
+
+                /*
+                Button(action: {
+                    
+                    let today = Date()
+                    print(today)
+
+                    //Add the days in reverse order
+
+                    dayData.add_day_test(Calendar.current.date(byAdding: .hour, value: -0, to: today)!, 110.59, 80.73, 166.0, 100.0, "hunger");
+                    dayData.add_day_test(Calendar.current.date(byAdding: .hour, value: -24, to: today)!, 110.59, 80.73, 166.0, 100.0, "hunger");
+                    dayData.add_day_test(Calendar.current.date(byAdding: .hour, value: -48, to: today)!, 110.59, 80.73, 166.0, 100, "hunger");
+                    dayData.add_day_test(Calendar.current.date(byAdding: .hour, value: -72, to: today)!, 110.59, 80.73, 166.0, 100, "hunger");
+                    dayData.add_day_test(Calendar.current.date(byAdding: .hour, value: -96, to: today)!, 110.59, 80.73, 165.0, 100, "hunger");
+                    dayData.add_day_test(Calendar.current.date(byAdding: .hour, value: -120, to: today)!, 110.59, 80.73, 165.0, 100, "hunger");
+                    dayData.add_day_test(Calendar.current.date(byAdding: .hour, value: -144, to: today)!, 110.59, 80.73, 165.0, 100, "hunger");
+                    
+
+
+    
+                    
+                    
+                }) {
+                    Text("Test Input")
+                }
+                 */
+                
+                Text("Personal Health Monitoring System").foregroundColor(Color.blue);
+                
+                NavigationLink(
+                    destination: ContentView(),
+                    label: {
+                        Text("Enter Data")
+                    }).buttonStyle(.borderedProminent)
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarHidden(true)
+                
+                NavigationLink(
+                    destination: ContentView(),
+                    label: {
+                        Text("View My Health")
+                    }).buttonStyle(.borderedProminent)
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarHidden(true)
+                
+                NavigationLink(
+                    destination: ContentView(),
+                    label: {
+                        Text("(USE THIS FOR TESTING DATA)")
+                    }).buttonStyle(.borderedProminent)
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarHidden(true)
+                
+                Spacer()
+                Spacer()
+            }
+                
+            }
     }
 }
 
