83a5aea454d8669b23a73d67194133e90f9a393e
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-18 16:00:33 -0700

    add all files

---
+++ CSE335Homework2/CSE335Homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  CSE335Homework2App.swift
+//  CSE335Homework2
+//
+//  Created by Arjun Dadhwal on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CSE335Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
